51f7dc5358db47107dd8db1fb5157396a577270c
Moa <moa3@gmail.comd../>
2022-05-16 01:07:06 +0530

    initial version

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dicta est doloremque tempora fuga ea, provident natus vel error sit quia consequatur. Ducimus fuga voluptas quis neque dolor eius saepe, enim earum atque, explicabo quod, cum esse ab aliquid? Saepe.</p>
+    </section>
+  )
+}
+
+export default About
